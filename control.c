587e750f517ddb8b2d7cfd1770f5e6a562cf2d3b
Jason Lin <jasonlin43212@gmail.com>
2018-12-12 15:23:06 -0500

    added some things

--- control.c
+++ control.c
@@ -5,8 +5,14 @@
 #include <sys/types.h>
 #include <sys/ipc.h>
 
-int main(){
+int main(int argc, char * argv[]){
 
+  if (argc == 0){
+    printf("You did not enter any arguments.");
+    exit(0);
+  }
+  char * argument = argv[0];
+  printf("%s",argument);
 
   return 0;
 }
